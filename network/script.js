d2705311918e1d521ce239583edfb9d7063cf90a
joy <42256457+joyhong85@users.noreply.github.com>
2022-07-17 21:25:28 +0900

    Update script.js

--- network/script.js
+++ network/script.js
@@ -3,62 +3,62 @@
   	$("#default").click(function (){
       $("button.cateBtn").removeClass("on");
       $(this).addClass("on");
-      document.getElementById("iframeDisplay").src = "https://joyhong85.github.io/network/centrality_default.html";
+      document.getElementById("iframeDisplay").src = "https://joyhong85.github.io/network/centrality/centrality_default.html";
     });
     $("#edge_weight").click(function (){
       $("button.cateBtn").removeClass("on");
       $(this).addClass("on");
-      document.getElementById("iframeDisplay").src = "https://joyhong85.github.io/network/edge_weight.html";
+      document.getElementById("iframeDisplay").src = "https://joyhong85.github.io/network/centrality/edge_weight.html";
     });
     $("#degree").click(function (){
       $("button.cateBtn").removeClass("on");
       $(this).addClass("on");
-      document.getElementById("iframeDisplay").src = "https://joyhong85.github.io/network/centrality_degree.html";
+      document.getElementById("iframeDisplay").src = "https://joyhong85.github.io/network/centrality/centrality_degree.html";
     });
     $("#weighted_degree").click(function (){
       $("button.cateBtn").removeClass("on");
       $(this).addClass("on");
-      document.getElementById("iframeDisplay").src = "https://joyhong85.github.io/network/centrality_weighted_degree.html";
+      document.getElementById("iframeDisplay").src = "https://joyhong85.github.io/network/centrality/centrality_weighted_degree.html";
     });
     $("#betweenness").click(function (){
       $("button.cateBtn").removeClass("on");
       $(this).addClass("on");
-      document.getElementById("iframeDisplay").src = "https://joyhong85.github.io/network/centrality_betweenness.html";
+      document.getElementById("iframeDisplay").src = "https://joyhong85.github.io/network/centrality/centrality_betweenness.html";
     });
     $("#edge_betweenness").click(function (){
       $("button.cateBtn").removeClass("on");
       $(this).addClass("on");
-      document.getElementById("iframeDisplay").src = "https://joyhong85.github.io/network/centrality_edge_betweenness.html";
+      document.getElementById("iframeDisplay").src = "https://joyhong85.github.io/network/centrality/centrality_edge_betweenness.html";
     });
     $("#closeness").click(function (){
       $("button.cateBtn").removeClass("on");
       $(this).addClass("on");
-      document.getElementById("iframeDisplay").src = "https://joyhong85.github.io/network/centrality_closeness.html";
+      document.getElementById("iframeDisplay").src = "https://joyhong85.github.io/network/centrality/centrality_closeness.html";
     });
     $("#eigenvector").click(function (){
       $("button.cateBtn").removeClass("on");
       $(this).addClass("on");
-      document.getElementById("iframeDisplay").src = "https://joyhong85.github.io/network/centrality_eigenvector.html";
+      document.getElementById("iframeDisplay").src = "https://joyhong85.github.io/network/centrality/centrality_eigenvector.html";
     });
     $("#weighted_eigenvector").click(function (){
       $("button.cateBtn").removeClass("on");
       $(this).addClass("on");
-      document.getElementById("iframeDisplay").src = "https://joyhong85.github.io/network/centrality_weighted_eigenvector.html";
+      document.getElementById("iframeDisplay").src = "https://joyhong85.github.io/network/centrality/centrality_weighted_eigenvector.html";
     });
     $("#harmonic").click(function (){
       $("button.cateBtn").removeClass("on");
       $(this).addClass("on");
-      document.getElementById("iframeDisplay").src = "https://joyhong85.github.io/network/centrality_harmonic.html";
+      document.getElementById("iframeDisplay").src = "https://joyhong85.github.io/network/centrality/centrality_harmonic.html";
     });
     $("#katz").click(function (){
       $("button.cateBtn").removeClass("on");
       $(this).addClass("on");
-      document.getElementById("iframeDisplay").src = "https://joyhong85.github.io/network/centrality_katz.html";
+      document.getElementById("iframeDisplay").src = "https://joyhong85.github.io/network/centrality/centrality_katz.html";
     });
     $("#pagerank").click(function (){
       $("button.cateBtn").removeClass("on");
       $(this).addClass("on");
-      document.getElementById("iframeDisplay").src = "https://joyhong85.github.io/network/centrality_pagerank.html";
+      document.getElementById("iframeDisplay").src = "https://joyhong85.github.io/network/centrality/centrality_pagerank.html";
     });
 });
 
